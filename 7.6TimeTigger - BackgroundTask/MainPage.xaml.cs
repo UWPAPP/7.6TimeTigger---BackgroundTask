d6a7dbcf0237a891ffe043d4ae0b20863bd8e624
martin <martin@V-DOXIE-W10>
2016-07-06 17:50:41 +0800

    Add project files.

---
+++ 7.6TimeTigger - BackgroundTask/MainPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _7._6TimeTigger___BackgroundTask
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ApplicationTrigger trigger = null;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        //注册后台任务
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var exampleTaskName = "ExampleBackgroundTask";
+            //如果后台任务已经存在，则跳出
+            foreach (var mytask in BackgroundTaskRegistration.AllTasks)
+            {
+                if (mytask.Value.Name == exampleTaskName)
+                {
+                    break;
+                }
+            }
+
+            trigger = new ApplicationTrigger();
+
+            //否则注册后台任务
+            var builder = new BackgroundTaskBuilder();
+            builder.Name = exampleTaskName;
+            builder.TaskEntryPoint = "RuntimeTask.Task";
+
+            builder.SetTrigger(trigger);
+
+            //指定后台任务的回调方法
+            BackgroundTaskRegistration task = builder.Register();
+            task.Progress += new BackgroundTaskProgressEventHandler(OnProgress);
+            task.Completed += new BackgroundTaskCompletedEventHandler(OnCompleted);
+
+        }
+
+        //取消后台任务的方法（这里取消所有的后台任务）
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+            {
+                cur.Value.Unregister(true);
+            }
+        }
+
+
+        //后台任务运行过程中执行的方法
+        private void OnProgress(IBackgroundTaskRegistration task, BackgroundTaskProgressEventArgs args)
+        {
+            //异步处理结果
+            var ignored = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                var progress = "Progress: " + args.Progress + "%";
+            });
+        }
+
+
+        //后台任务完成执行的方法
+        private void OnCompleted(IBackgroundTaskRegistration task, BackgroundTaskCompletedEventArgs args)
+        {
+            var settings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            var key = task.Name;
+            var message = settings.Values[key].ToString();
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //Signal the ApplicationTrigger
+            var result = await trigger.RequestAsync();
+            Debug.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ RuntimeTask/Task.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Storage;
+using Windows.System.Threading;
+
+namespace RuntimeTask
+{
+    public sealed partial class Task : IBackgroundTask
+    {
+        //后台任务取消的原因
+        BackgroundTaskCancellationReason _cancelReason = BackgroundTaskCancellationReason.Abort;
+        volatile bool _cancelRequested = false;
+        BackgroundTaskDeferral _deferral = null;
+        ThreadPoolTimer _periodicTimer = null;
+        uint _progress = 0;
+        IBackgroundTaskInstance _taskInstance = null;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            Debug.WriteLine("Background " + taskInstance.Task.Name + " Starting...");
+
+            //获取后台任务的级别
+            var cost = BackgroundWorkCost.CurrentBackgroundWorkCost;
+            var settings = ApplicationData.Current.LocalSettings;
+            settings.Values["BackgroundWorkCost"] = cost.ToString();
+
+
+            //后台任务取消的事件
+            taskInstance.Canceled += new BackgroundTaskCanceledEventHandler(OnCanceled);
+
+            //deferral
+            _deferral = taskInstance.GetDeferral();
+
+            //后台任务实例
+            _taskInstance = taskInstance;
+
+            //定时器
+            _periodicTimer = ThreadPoolTimer.CreatePeriodicTimer(new TimerElapsedHandler(PeriodicTimerCallback), TimeSpan.FromSeconds(1));
+        }
+
+        //后台任务取消
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            _cancelRequested = true;
+            _cancelReason = reason;
+
+            Debug.WriteLine("Background " + sender.Task.Name + " Cancel Requested...");
+        }
+
+
+        //定时器执行的方法
+        private void PeriodicTimerCallback(ThreadPoolTimer timer)
+        {
+            //如果没有被取消，任务没有完成，则执行后台任务的Progress方法
+            if ((_cancelRequested == false) && (_progress < 100))
+            {
+                _progress += 10;
+                _taskInstance.Progress = _progress;
+            }
+            //如果任务完成，取消定时器，并告知后台任务完成
+            else
+            {
+                //取消定时器
+                _periodicTimer.Cancel();
+                //获取后台任务的名称
+                var key = _taskInstance.Task.Name;
+                //如果任务没有完成，则存储取消任务的原因，如果任务完成，则存储Completed
+                String taskStatus = (_progress < 100) ? "Canceled with reason: " + _cancelReason.ToString() : "Completed";
+                var settings = ApplicationData.Current.LocalSettings;
+                settings.Values[key] = taskStatus;
+                Debug.WriteLine("Background " + _taskInstance.Task.Name + settings.Values[key]);
+
+                //告知后台任务完成
+                _deferral.Complete();
+            }
+        }
+    }
+
+}
